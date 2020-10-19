6a54700d0482f4ba6d38c9d9ce95ac9e2ab477a0
Shanzay160 <shanzaytariq186@gmail.com>
2020-10-19 23:50:49 +0500

    javaprograms is added

---
+++ Test5.java
@@ -0,0 +1,10 @@
+public class Test5 {
+  static int myMethod(int x) {
+    return 5 + x;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(myMethod(3));
+  }
+}
+// Outputs 8 (5 + 3)
